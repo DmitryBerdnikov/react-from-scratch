4fd0a7cd8534db89eca0eeb2546eabfe5ec7e7b2
Dmitry Berdnikov <berdnikovdmitry@mail.ru>
2022-03-12 18:30:27 +0300

    add updateDomElement

--- todo-app.js
+++ todo-app.js
@@ -3,11 +3,13 @@
 const root = document.getElementById('root');
 
 const App = (
-  <div id="test">
-    <p><span>One</span><span>Two</span></p>
+  <div id="test" onClick={() => console.log('test')}>
+    <p className="text"><span>One</span><span>Two</span></p>
     {2 === 1 && <div>False</div>}
     simple text
   </div>
 );
+
+console.log(App)
 
 FakeReact.render(App, root);
