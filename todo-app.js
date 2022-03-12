e58af6afe5bb6effa609cd1378348f14ed54097b
Dmitry Berdnikov <berdnikovdmitry@mail.ru>
2022-03-12 19:50:20 +0300

    add diff

--- todo-app.js
+++ todo-app.js
@@ -2,14 +2,27 @@
 
 const root = document.getElementById('root');
 
-const App = (
+const App1 = (
   <div id="test" onClick={() => console.log('test')}>
+    <div>Test</div>
     <p className="text"><span>One</span><span>Two</span></p>
     {2 === 1 && <div>False</div>}
     simple text
   </div>
 );
 
-console.log(App)
+FakeReact.render(App1, root);
 
-FakeReact.render(App, root);
+setTimeout(() => {
+  console.log('re-rendering');
+
+  const App2 = (
+    <div id="test" onClick={() => console.log('test')}>
+      <p style="background: red;" className="bar"><span>One</span><span>Two</span></p>
+      {2 === 1 && <div>False</div>}
+      new simple text
+    </div>
+  );
+  
+  FakeReact.render(App2, root);
+}, 1000);
