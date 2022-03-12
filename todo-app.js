b4bc1f61ca255b2366baf1e83c6232e489d4b60e
Dmitry Berdnikov <berdnikovdmitry@mail.ru>
2022-03-12 17:44:56 +0300

    refactor

--- todo-app.js
+++ todo-app.js
@@ -1,11 +1,12 @@
-/** @jsx Fakeact.createElement */
+/** @jsx FakeReact.createElement */
 
 const root = document.getElementById('root');
 
 const App = (
-  <div>
+  <div id="test">
     <p><span>Boo</span></p>
     {2 === 1 && <div>False</div>}
+    simple text
   </div>
 );
 
