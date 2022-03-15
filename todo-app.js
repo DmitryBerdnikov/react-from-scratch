ee13dabf71ffe10743ece0720aa21b6767cca7fc
Dmitry Berdnikov <berdnikovdmitry@mail.ru>
2022-03-15 22:16:53 +0300

    add simple functional component render

--- todo-app.js
+++ todo-app.js
@@ -2,26 +2,30 @@
 
 const root = document.getElementById('root');
 
-const App1 = (
-  <div id="test" onClick={() => console.log('test')}>
-    <p className="text"><span>One</span><span>Two</span></p>
-    {2 === 1 && <div>False</div>}
-    simple text
-  </div>
-);
+// const App1 = (
+//   <div id="test" onClick={() => console.log('test')}>
+//     <p className="text"><span>One</span><span>Two</span></p>
+//     {2 === 1 && <div>False</div>}
+//     simple text
+//   </div>
+// );
 
-FakeReact.render(App1, root);
+// FakeReact.render(App1, root);
 
-setTimeout(() => {
-  console.log('re-rendering');
+// setTimeout(() => {
+//   console.log('re-rendering');
 
-  const App2 = (
-    <div id="test" onClick={() => console.log('test')}>
-      <p style="background: red;" className="bar"><span>One</span><span>Two</span></p>
-      {2 === 1 && <div>False</div>}
-      new simple text
-    </div>
-  );
+//   const App2 = (
+//     <div id="test" onClick={() => console.log('test')}>
+//       <p style="background: red;" className="bar"><span>One</span><span>Two</span></p>
+//       {2 === 1 && <div>False</div>}
+//       new simple text
+//     </div>
+//   );
   
-  FakeReact.render(App2, root);
-}, 1000);
+//   FakeReact.render(App2, root);
+// }, 1000);
+
+const Heart = (props) => <span style={props.style}>test</span>;
+
+FakeReact.render(<Heart style="color: red;" />, root);
